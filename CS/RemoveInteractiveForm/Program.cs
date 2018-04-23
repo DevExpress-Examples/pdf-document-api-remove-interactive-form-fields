9340b21fab97c09d226f63e0e4ad608f04d51785
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RemoveInteractiveForm/Program.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.Pdf;
+using System;
+
+namespace RemoveInteractiveForm {
+    class Program {
+        static void Main(string[] args) {
+            using (PdfDocumentProcessor processor = new PdfDocumentProcessor()) {
+
+                // Load a document with an interactive form.
+                processor.LoadDocument("..\\..\\InteractiveForm.pdf");
+
+                // Remove a form field by its name.
+                if (processor.RemoveFormField("FirstName"))
+
+                    // Save the modified document.
+                    processor.SaveDocument("..\\..\\Result1.pdf");
+
+                else
+
+                    // Show a message if the form field was not found in a document.
+                    Console.WriteLine("The form field was not removed from a document. Make sure, the form field exists in the document.");
+
+                // Remove the whole interactive form.
+                if (processor.RemoveForm())
+
+                    // Save the modified document.
+                    processor.SaveDocument("..\\..\\Result2.pdf");
+
+                else
+
+                    // Show a message if the interactive form was not found in a document.
+                    Console.WriteLine("The interactive form was not removed from a document. Make sure the interactive form exists in the document.");
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to delete interactive form fields from a PDF document
+
+
+This example shows how to remove a particular form field and a whole interactive form from a PDF document.
+
+
+<h3>Description</h3>
+
+To remove all form fields from a document, call the&nbsp;<a href="https://documentation.devexpress.com/#DocumentServer/DevExpressPdfPdfDocumentProcessor_RemoveFormtopic">PdfDocumentProcessor.RemoveForm</a> method. <br>To remove a particular form field by its name, call the<strong>&nbsp;</strong><a href="https://documentation.devexpress.com/#DocumentServer/DevExpressPdfPdfDocumentProcessor_RemoveFormFieldtopic">PdfDocumentProcessor.RemoveFormField</a>  method and pass a field name as an argument to this method.
+
+<br/>
+
+
